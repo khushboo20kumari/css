34485232910cc599366b7b825bfa94d26590deab
Khushboo Kumari <khushboo20@navgurukul.org>
2023-11-16 11:53:11 +0530

    z-index

---
+++ css/src/App.js
@@ -0,0 +1,13 @@
+import Zindex from './Z-index';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Zindex/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ css/src/Z-index.js
@@ -0,0 +1,14 @@
+function Zindex(){
+   return(
+    <>
+    <div>
+        <div className="box1" style={{background:"black",height:"200px",width:"200px"}}>box1</div>
+        <div className="box2" style={{background:"blue",height:"200px",width:"200px"}}>box1</div>
+        <div className="box3" style={{background:"pink",height:"200px",width:"200px"}}>box1</div>
+        <div className="box4" style={{background:"green",height:"200px",width:"200px"}}>box1</div>
+        
+    </div>
+    </>
+   )
+}
+export default Zindex;
