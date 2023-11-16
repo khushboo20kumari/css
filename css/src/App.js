003e05c4240fb8ee77c5204595091b445695f2e7
Khushboo Kumari <khushboo20@navgurukul.org>
2023-11-16 14:15:03 +0530

    overFLow

--- css/src/App.js
+++ css/src/App.js
@@ -1,10 +1,11 @@
-import Zindex from './Z-index';
+// import Zindex from './Z-index';
 import './App.css';
-
+import OverFlow from './OverFlow';
 function App() {
   return (
     <div className="App">
-      <Zindex/>
+      {/* <Zindex/> */}
+      <OverFlow/>
      
     </div>
   );
